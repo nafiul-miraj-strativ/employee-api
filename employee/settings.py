4cc5f4b89a22b80decd4bc527896a8793d045861
Nafiul Alam Miraj <nafiulmiraj@Nafiuls-MacBook-Air-2.local>
2025-04-30 15:45:17 +0600

    fixed database

--- employee/settings.py
+++ employee/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'employee.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject2',
-        'USER': 'postgres',
-        'PASSWORD': 'strativ',
+        'NAME': 'myproject',
+        'USER': 'myuser',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
